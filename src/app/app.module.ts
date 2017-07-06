e64940b2faa8fc14dffe261db5703639cd1a6208
Kim Turley <kim@bynd.com>
2017-07-06 18:27:59 -0400

    Load config before app init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
@@ -6,13 +6,20 @@ import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 
-import { LOCALE_ID } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AppRoutingService } from './app-routing.service';
 import { ProductsModule } from './products/products.module';
+
+export let Settings = {
+    items : []
+};
+
+export function init(config: AppRoutingService) {
+    return () => config.getItems().then((items) => Settings.items = items);
+}
 
 @NgModule({
     declarations: [
@@ -30,7 +37,17 @@ import { ProductsModule } from './products/products.module';
         ProductsModule
     ],
     providers: [
-        { provide: LOCALE_ID, useValue: document['locale'] ? document['locale'] : 'en-US' }
+        {
+            provide: LOCALE_ID,
+            useValue: document['locale'] ? document['locale'] : 'en-US'
+        },
+        {
+            provide: APP_INITIALIZER,
+            useFactory: init,
+            deps: [AppRoutingService],
+            multi: true
+        },
+        AppRoutingService
     ],
     bootstrap: [AppComponent]
 })
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,33 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { NavigationService } from './navigation.service';
+import { Settings } from '../app.module';
 
 @Component({
     selector: 'app-navigation',
     templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.css'],
-    providers: [NavigationService]
+    styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
     items: Array<any>;
 
     constructor(
-        private router: Router,
-        private navigationService: NavigationService
+        private router: Router
     ) { }
 
     ngOnInit() {
+        console.log('NavigationComponent', Settings);
+        this.items = Settings.items;
         const routerConfig = this.router.config;
-        this.navigationService.getItems()
-            .subscribe((items) => {
-                items.forEach((item) => {
-                    routerConfig.push(item);
-                });
-                this.items = items;
-                this.router.resetConfig(routerConfig);
-                console.log('navigationService.subscribe', items, routerConfig);
-            })
+        this.items.forEach((item) => {
+            routerConfig.push(item);
+        });
+        this.router.resetConfig(routerConfig);
     }
 
 }
