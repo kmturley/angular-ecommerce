35a0aa819c05b8990f55d6e09c5d069d39803d32
Kim Turley <kim@bynd.com>
2017-07-11 09:34:31 -0400

    Load product view async

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Router  } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+    private url = 'assets/data/products/iphone.json';
+    item: Observable<any[]>;
 
-  constructor() { }
+    constructor(
+        private http: Http,
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.item = this.http.get(this.url)
+            .map((res: Response) => res.json());
+    }
 
 }
