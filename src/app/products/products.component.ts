b321cc3981603026edecd934fd1adf733f9ab25c
Kim T <kimturley@gmail.com>
2017-07-21 17:05:25 -0400

    Added missing dependencies

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
     selector: 'app-products',
