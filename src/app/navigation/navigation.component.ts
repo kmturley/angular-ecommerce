1f4b0da43451cfef4134d110efd0287cebe231f0
Kim Turley <kim@bynd.com>
2017-08-03 14:13:16 -0400

    Language switcher

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { routes } from 'app/app.module';
 
 @Component({
@@ -13,11 +13,13 @@ export class NavigationComponent implements OnInit {
     language: string;
 
     constructor(
-        private router: Router
+        private router: Router,
+        private location: Location
     ) { }
 
     ngOnInit() {
-        this.language = 'en';
+        const paths = document.getElementsByTagName('base')[0].href.replace(/\/$/, '').split('/');
+        this.language = paths[paths.length - 1].length === 2 ? paths[paths.length - 1] : 'en';
         this.items = routes;
         const routerConfig = this.router.config;
         this.items.forEach((item) => {
@@ -27,7 +29,9 @@ export class NavigationComponent implements OnInit {
     }
 
     onChange() {
-        window.location.href = document.getElementsByTagName('base')[0].href + this.language + '/';
+        const paths = document.getElementsByTagName('base')[0].href.replace(/\/$/, '').split('/');
+        paths.pop();
+        window.location.href = paths.join('/') + '/' + this.language;
     }
 
 }
