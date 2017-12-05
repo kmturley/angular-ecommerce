070f4aec1924d43f9d29cd717e5c601887acbeb6
Kim Turley <kim@bynd.com>
2017-12-04 19:22:02 -0500

    Upgrade angular version

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,10 +7,8 @@ describe('angular-ecommerce App', () => {
     page = new AngularEcommercePage();
   });
 
-  it('should display welcome message', done => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    page.getParagraphText()
-      .then(msg => expect(msg).toEqual('Welcome to app!!'))
-      .then(done, done.fail);
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
